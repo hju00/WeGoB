9d5f7b0793f1f9e919940bd30eb8a04ab4b8990d
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-08-06 22:18:47 +0900

    update solve problem

--- 250806/10282_송주헌.java
+++ 250806/10282_송주헌.java
@@ -3,48 +3,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class 10282_송주헌 {
+// List 초기화할 때 간선 개수만큼 초기화 해줬는데
+// 그래서 메모리 초과가 났던거였네요
+public class Main {
     private static int n;
     private static int c;
-
-    private static final int INF = 1_000_000_000;
 
     private static int[] dist;
     private static List<List<Node>> graph;
 
     static class Node{
-        int idx;
-        int c;
+        int dest;
+        int time;
 
-        Node(int idx, int s){
-            this.idx = idx;
-            this.c = s;
+        Node(int dest, int s){
+            this.dest = dest;
+            this.time = s;
         }
     }
     
     private static void dijkstra(){
-        PriorityQueue<Node> q = new PriorityQueue<>(Comparator.comparingInt(o -> o.c));
-		
-        Arrays.fill(dist, INF);
         boolean[] visited = new boolean[n+1];
-        q.offer(new Node(c, 0));
-		
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-			
-            if (!visited[cur.idx]) {
-                visited[cur.idx] = true;
-				
-                for (Node next : graph.get(cur.idx)) {
-                    if (dist[next.idx] > dist[cur.idx] + next.c) {
-                        dist[next.idx] = dist[cur.idx] + next.c;
-                        q.offer(new Node(next.idx, dist[next.idx]));
-                    }
+        int[] dist = Main.dist;
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        visited[c] = true;
+        dist[c] = 0;
+        for(int i = 0; i < n+1; i++){
+            int time = Integer.MAX_VALUE;
+            int idx = c;
+
+            // 현재 최소값 찾기
+            for(int j = 1; j < n+1; j++){
+                if(!visited[j] && dist[j] < time){
+                    time = dist[j];
+                    idx = j;
+                }
+            }
+
+            visited[idx] = true;
+
+            // 최소 거리 갱신
+            for(int j = 0; j < graph.get(idx).size(); j++){
+                Node cur = graph.get(idx).get(j);
+
+                if(dist[cur.dest] > dist[idx] + cur.time){
+                    dist[cur.dest] = dist[idx] + cur.time;
                 }
             }
         }
@@ -62,8 +69,9 @@ public class 10282_송주헌 {
             c = Integer.parseInt(st.nextToken());
 
             graph = new ArrayList<>(n+1);
+            dist = new int[n+1];
             for(int i = 0; i < n+1; i++){
-                graph.add(new ArrayList<>(d));
+                graph.add(new ArrayList<>());
             }
             
             while(d-- > 0){
@@ -76,18 +84,16 @@ public class 10282_송주헌 {
                 graph.get(b).add(new Node(a, s));
             }
 
-            dist = new int[n+1];
             dijkstra();
 
             int cnt = 1;
             int max = 0;
-
             for(int i = 1; i < n+1; i++){
                 if(i == c) continue;
 
                 int cur = dist[i];
 
-                if(cur < INF){
+                if(cur < Integer.MAX_VALUE){
                     max= Math.max(max, cur);
                     cnt++;
                 }
