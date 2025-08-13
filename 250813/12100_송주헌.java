07e5b1b254d94390357c118b7cc304109e41e835
sjh1108 <55789727+sjh1108@users.noreply.github.com>
2025-08-13 23:11:04 +0900

    .

--- 250813/12100_송주헌.java
+++ 250813/12100_송주헌.java
@@ -6,94 +6,33 @@ public class Main {
 
     private static int max = 0;
 
-    private static int[][] map;
-    private static int[][] copyMap;
-
     // 맵의 최대값을 저장하기 위한 메소드
-    private static void getMax(){
+    private static void getMax(int[][] map) {
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 max = Math.max(max, map[i][j]);
             }
         }
     }
-    
-    // 각 방향으로 이동하는 메소드들
-    private static void goLeft(){
+
+    private static int[][] rotate(int[][] map) {
+        int[][] rotated = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                rotated[j][N - 1 - i] = map[i][j];
+            }
+        }
+
+        return rotated;
+    }
+
+    private static void pressMap(int[][] map){
         for(int i = 0; i < N; i++){
             int[] t = new int[N];
-            int idx = 0;
 
+            int idx = 0;
             for(int j = 0; j < N; j++){
-                if(map[i][j] == 0) continue;
-
-                if(t[idx] == 0) {
-                    t[idx] = map[i][j];
-                } else if(t[idx] == map[i][j]) {
-                    t[idx] *= 2; // 값을 합치고
-                    idx++;       // 다음 위치로 이동
-                } else {
-                    idx++;       // 다음 위치로 이동하고
-                    t[idx] = map[i][j]; // 값을 놓음
-                }
-            }
-            map[i] = t;
-        }
-    }
-    private static void goRight(){
-        for(int i = 0; i < N; i++){
-            int[] t = new int[N];
-
-            int idx = N-1;
-            for(int j = N-1; j >= 0; j--){
-                if(map[i][j] == 0) continue;
-
-                if(t[idx] == 0){
-                    t[idx] = map[i][j];
-                }
-                else if(t[idx] == map[i][j]){
-                    t[idx] *= 2;
-                    idx--;
-                }else{
-                    idx--;
-                    t[idx] = map[i][j];
-                }
-            }
-
-            map[i] = t;
-        }
-
-    }
-    private static void goUp(){
-        for(int j = 0; j < N; j++){
-            int[] t = new int[N];
-            int idx = 0;
-
-            for(int i = 0; i < N; i++){
-                if(map[i][j] == 0) continue;
-
-                if(t[idx] == 0) {
-                    t[idx] = map[i][j];
-                } else if(t[idx] == map[i][j]) {
-                    t[idx] *= 2;
-                    idx++;
-                } else {
-                    idx++;
-                    t[idx] = map[i][j];
-                }
-            }
-
-            for(int i = 0; i < N; i++){
-                map[i][j] = t[i];
-            }
-        }
-    }
-    private static void goDown(){
-        for(int i = 0; i < N; i++){
-            int[] t = new int[N];
-
-            int idx = N-1;
-            for(int j = N-1; j >= 0; j--){
                 if(map[j][i] == 0) continue;
 
                 if(t[idx] == 0){
@@ -101,9 +40,9 @@ public class Main {
                 }
                 else if(t[idx] == map[j][i]){
                     t[idx] *= 2;
-                    idx--;
+                    idx++;
                 }else{
-                    idx--;
+                    idx++;
                     t[idx] = map[j][i];
                 }
             }
@@ -114,47 +53,19 @@ public class Main {
         }
     }
 
-    // 코드 가독성을 위한 메소드 분리
-    private static void move(int d){
-        switch(d){
-            case 0:
-                goLeft();
-                return;
-            case 1:
-                goRight();
-                return;
-            case 2:
-                goUp();
-                return;
-            case 3:
-                goDown();
-                return;
-        }
-    }
-
-    private static void dfs(int depth){
-        // 깊이가 5가 되면 최대값을 갱신하고 종료
+    private static void dfs(int[][] map, int depth){
         if(depth == 5) {
-            getMax();
+            getMax(map);
             return;
         }
 
-        // 현재 맵을 복사하여 저장
-        for(int i = 0; i < N; i++) {
-            copyMap[i] = Arrays.copyOfRange(map[i], 0, N);
-        }
+        for (int i = 0; i < 4; i++) {
+            int[][] newMap = rotate(map);
 
-        for(int d = 0; d < 4; d++){
-            // 방향에 따라 이동
-            move(d);
+            pressMap(newMap);
+            dfs(newMap, depth + 1);
 
-            // 이동 후 depth + 1로 dfs 호출
-            dfs(depth + 1);
-
-            // 저장했던 맵으로 되돌리기
-            for(int i = 0; i < N; i++) {
-                map[i] = Arrays.copyOfRange(copyMap[i], 0, N);
-            }
+            map = rotate(map);
         }
     }
 
@@ -163,8 +74,7 @@ public class Main {
         
         N = Integer.parseInt(br.readLine());
         
-        map = new int[N][N];
-        copyMap = new int[N][];
+        int[][] map = new int[N][N];
         
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
@@ -174,9 +84,8 @@ public class Main {
             }
         }
 
-        dfs(0);
+        dfs(map, 0);
 
-        
         System.out.println(max);
     }
 }
